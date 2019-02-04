3d1619210ca460d3d10a26c747e431ecd9e7e1ce
maurod97 <39266545+maurod97@users.noreply.github.com>
2019-02-04 20:52:35 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#sudo pacman -S --noconfirm rofi;
+sudo pacman -S --noconfirm ncmpcpp;
+sudo pacman -S --noconfirm tldr++;
+sudo pacman -S --noconfirm franz;
+sudo pacman -S --noconfirm polybar && sudo pacman -S --noconfirm weather-bar; #https://github.com/tobi-wan-kenobi/bumblebee-status/wiki/Available-Modules
+sudo pacman -S --noconfirm syncthing;
+sudo pacman -S --noconfirm calibre-git;
+sudo pacman -S --noconfirm zathura-git && sudo pacman -S --noconfirm zathura-pdf-poppler-git;
+sudo pacman -S --noconfirm neofetch;
+#sudo pacman -S --noconfirm htop;
+sudo pacman -S --noconfirm firefox;
+
+
+
+
+
+
+
